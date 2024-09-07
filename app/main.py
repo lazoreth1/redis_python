451a7c9e3a98e72207a11f9629a9546e2f3dec50
lazoreth <lazoreth@gmail.com>
2024-09-07 21:58:49 +0300

    PING

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket  # noqa: F401
+from dataclasses import dataclass
 
 
 def handle_set(data):
@@ -10,6 +11,7 @@ def handle_unset(data):
 
 
 def handle_ping(data):
+    print("ping_func")
     return "+PONG\r\n"
 
 
@@ -20,6 +22,58 @@ commands = {
 }
 
 
+class Type:
+    pass
+
+
+@dataclass
+class Request:
+    command: str
+    type: Type
+    data: list
+
+
+@dataclass
+class Array(Type):
+    array_len: int
+
+
+def parse_type(s: str):
+    t = None
+    match s[0]:
+        case "*":
+            t = Array(array_len=int(s[1]))
+    return t
+
+
+def parse_command(s: str):
+    print(f"command: {s}")
+    match s:
+        case "PING":
+            return "PING"
+
+
+def parse_request(data: bytes):
+    r = data.decode().split()
+    print(r)
+    request_type = parse_type(r[0])
+    r = r[1:]
+    command = parse_command(r[1])
+    r = r[2:]
+    req = Request(command=command, type=request_type, data=r)
+    print(req)
+    match req.command:
+        case "PING":
+            return "+PONG"
+    # match req.type:
+    #     case Array():
+    #         for i in range(1, len(r), 2):
+    #             l = r[i - 1]
+    #             word_len = int(l[1:])
+    #             word = r[i]
+    #             print(f"Word: {word}, Len: {word_len}")
+
+
 def handle_command(sock):
     try:
         while True:
@@ -27,13 +81,11 @@ def handle_command(sock):
             data = sock.recv(1024)
             if not data:
                 break
-            command = data.decode().strip().split()[0]
-            command_func = commands.get(command)
-            if not command_func:
-                response = "Unknown command"
-            response = command_func(data)
+            print(f"Got: {data.decode()}")
+            response = parse_request(data) + "\r\n"
+            print("parsed")
             sock.send(response.encode())
-            print(data)
+            print("sent")
     except ConnectionResetError:
         print("Connection closed")
         return
